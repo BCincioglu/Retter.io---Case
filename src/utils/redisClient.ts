53445b97b1124a32054ebd86a5e2183b80bdea06
bcincioglu <baris.cincioglu@outlook.com>
2024-12-24 02:26:21 +0300

    trim comments

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from 'redis';
 
-// Redis istemcisi oluşturma
 const redisClient = createClient({
   socket: {
     host: process.env.REDIS_HOST || 'localhost',
@@ -8,7 +7,6 @@ const redisClient = createClient({
   },
 });
 
-// Bağlantı olayları
 redisClient.on('connect', () => {
   console.log('Connected to Redis');
 });
@@ -17,10 +15,9 @@ redisClient.on('error', (err) => {
   console.error('Redis error:', err);
 });
 
-// Redis istemcisini başlat
 (async () => {
   try {
-    await redisClient.connect(); // Bağlantıyı etkinleştir
+    await redisClient.connect();
   } catch (err) {
     console.error('Redis connection failed:', err);
   }
