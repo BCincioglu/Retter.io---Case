ee3bab97d92ac43854b9b2bcdcfaf991d69bdc2b
BCincioglu <168750017+BCincioglu@users.noreply.github.com>
2024-12-25 04:30:02 +0300

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,18 +1,15 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-// Kullanıcı Modeli için arayüz
 export interface IUser extends Document {
   userId: string;
   userName: string;
 }
 
-// Kullanıcı şeması
 const UserSchema: Schema = new Schema({
   userId: { type: String, required: true, unique: true },
   userName: { type: String, required: true },
 });
 
-// Modeli oluştur
 const User = mongoose.model<IUser>('User', UserSchema);
 
 export default User;
