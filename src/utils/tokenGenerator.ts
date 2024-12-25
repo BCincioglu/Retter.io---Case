a0eef738c89ce6d410e24a3f418baafd34449df7
BCincioglu <168750017+BCincioglu@users.noreply.github.com>
2024-12-25 04:31:30 +0300

    Update tokenGenerator.ts

--- src/utils/tokenGenerator.ts
+++ src/utils/tokenGenerator.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'default_secret'; // Güvenli bir anahtar kullanın
-const TOKEN_EXPIRATION = '1h'; // Token süresi (ör. 1 saat)
+const JWT_SECRET = process.env.JWT_SECRET || 'default_secret'; 
+const TOKEN_EXPIRATION = '1h'; 
 
 export const generateToken = (userId: string): string => {
   return jwt.sign({ userId }, JWT_SECRET, { expiresIn: TOKEN_EXPIRATION });
